bbd52c19f74921ea5272b618f91d4a25485fa1ca
John Liebenau <john.liebenau@nordstrom.com>
2023-06-26 13:54:20 -0700

    Project Setup

--- Components/Strata.Foundation.Core/main/java/strata/foundation/core/configuration/IConfiguration.java
+++ Components/Strata.Foundation.Core/main/java/strata/foundation/core/configuration/IConfiguration.java
@@ -4,14 +4,25 @@
 
 package strata.foundation.core.configuration;
 
+import strata.foundation.core.container.Pair;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
 public
 interface IConfiguration
 {
     String
     getProperty(String key);
 
+    Map<String,Object>
+    getProperties(String... prefixes);
+
     boolean
     hasProperty(String key);
+
+    Stream<Pair<String,Object>>
+    stream();
 }
 
 //////////////////////////////////////////////////////////////////////////////
--- Components/Strata.Foundation.Core/main/java/strata/foundation/core/configuration/SecureConfiguration.java
+++ Components/Strata.Foundation.Core/main/java/strata/foundation/core/configuration/SecureConfiguration.java
@@ -7,10 +7,15 @@ package strata.foundation.core.configuration;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.iv.RandomIvGenerator;
 import org.jasypt.properties.EncryptableProperties;
+import strata.foundation.core.container.Pair;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public
 class SecureConfiguration
@@ -34,10 +39,40 @@ class SecureConfiguration
     }
 
     @Override
+    public Map<String,Object>
+    getProperties(String... prefixes)
+    {
+        return
+            stream()
+                .filter(pair -> matchPrefixes(pair.getFirst(),prefixes))
+                .collect(
+                    Collectors.toMap(
+                        pair -> pair.getFirst(),
+                        pair -> pair.getSecond()));
+    }
+
+    @Override
     public boolean
     hasProperty(String key)
     {
         return itsProperties.containsKey(key);
+    }
+
+    @Override
+    public Stream<Pair<String,Object>>
+    stream()
+    {
+        return
+            itsProperties
+                .entrySet()
+                .stream()
+                .map(
+                    entry ->
+                        Pair.create(
+                            entry
+                                .getKey()
+                                .toString(),
+                            entry.getValue()));
     }
 
     protected StandardPBEStringEncryptor
@@ -56,6 +91,16 @@ class SecureConfiguration
     getPropertiesEncryptionKey()
     {
         return System.getenv("PROPERTIES_ENCRYPTION_KEY");
+    }
+
+    protected boolean
+    matchPrefixes(String key,String... prefixes)
+    {
+        return
+            Arrays
+                .stream(prefixes)
+                .anyMatch(prefix -> key.startsWith(prefix));
+
     }
 }
 
