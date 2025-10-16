21ca2738c55a19ac6837c6dd9abd2f4c1425d1ee
johnliebenau <johnliebenau@gmail.com>
2025-10-16 08:28:10 -0700

    Added CompletionStageMap and tests.

---
+++ Components/Strata.Foundation.Core/main/java/strata/foundation/core/concurrent/CompletionStageMap.java
@@ -0,0 +1,89 @@
+//////////////////////////////////////////////////////////////////////////////
+// CompletionStageMap.java
+//////////////////////////////////////////////////////////////////////////////
+
+package strata.foundation.core.concurrent;
+
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+
+public
+class CompletionStageMap<K,V>
+{
+    private final Map<K,CompletionStage<V>> pending;
+
+    public
+    CompletionStageMap()
+    {
+        pending = new ConcurrentHashMap<>();
+    }
+
+    public CompletionStage<V>
+    put(K key,CompletionStage<V> stage)
+    {
+        if (isPending(stage))
+        {
+            stage.whenComplete((x,e) -> remove(key));
+            pending.put(key,stage);
+        }
+
+        return stage;
+    }
+
+    public CompletionStage<V>
+    remove(K key)
+    {
+        if (containsKey(key) && isDone(pending.get(key)))
+            return pending.remove(key);
+
+        return null;
+    }
+
+    public CompletionStage<V>
+    get(K key) { return pending.get(key); }
+
+    public boolean
+    containsKey(K key)
+    {
+        return pending.containsKey(key);
+    }
+
+    public boolean
+    isPending(K key)
+    {
+        return pending.containsKey(key) && isPending(pending.get(key));
+    }
+
+    public boolean
+    isEmpty() { return pending.isEmpty(); }
+
+    public Map<K,V>
+    joinAll()
+    {
+        return
+            pending
+                .entrySet()
+                .stream()
+                .map(entry -> Map.entry(entry.getKey(),entry.getValue().toCompletableFuture()))
+                .map(entry -> Map.entry(entry.getKey(),entry.getValue().join()))
+                .collect(
+                    ConcurrentHashMap::new,
+                    (m,e) -> m.put(e.getKey(),e.getValue()),
+                    Map::putAll);
+    }
+
+    private boolean
+    isPending(CompletionStage<V> stage)
+    {
+        return !stage.toCompletableFuture().isDone();
+    }
+
+    public boolean
+    isDone(CompletionStage<V> stage)
+    {
+        return stage.toCompletableFuture().isDone();
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
---
+++ Tests/Strata.Foundation.CoreTest/test/java/strata/foundation/core/concurrent/CompletionStageMapTest.java
@@ -0,0 +1,153 @@
+/// ///////////////////////////////////////////////////////////////////////////
+// CompletionStageMapTest.java
+//////////////////////////////////////////////////////////////////////////////
+
+package strata.foundation.core.concurrent;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("CommitStage")
+public
+class CompletionStageMapTest
+{
+    private ExecutorService                   executor;
+    private CompletionStageMap<String,String> subject;
+
+    @BeforeEach
+    public void
+    setUp() throws Exception
+    {
+        executor = Executors.newCachedThreadPool();
+        subject  = new CompletionStageMap<>();
+    }
+
+    @Test
+    public void
+    testPutGet() throws Exception
+    {
+        CompletionStage<String> expectedA =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Apple",5000),executor);
+        CompletionStage<String> expectedB =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Banana",5000),executor);
+        CompletionStage<String> expectedC =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Cherry",5000),executor);
+
+        subject.put("A",expectedA);
+        subject.put("B",expectedB);
+        subject.put("C",expectedC);
+
+        assertEquals(expectedA,subject.get("A"));
+        assertEquals(expectedB,subject.get("B"));
+        assertEquals(expectedC,subject.get("C"));
+    }
+
+    @Test
+    public void
+    testRemoveIsEmpty() throws Exception
+    {
+        CompletionStage<String> expectedA =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Apple",50),executor);
+        CompletionStage<String> expectedB =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Banana",50),executor);
+        CompletionStage<String> expectedC =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Cherry",50),executor);
+
+        subject.put("A",expectedA);
+        subject.put("B",expectedB);
+        subject.put("C",expectedC);
+
+        assertFalse(subject.isEmpty());
+
+        Thread.sleep(100);
+
+        assertTrue(subject.isEmpty());
+
+    }
+
+    @Test
+    public void
+    testJoinAll() throws Exception
+    {
+        CompletionStage<String> expectedA =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Apple",5000),executor);
+        CompletionStage<String> expectedB =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Banana",7500),executor);
+        CompletionStage<String> expectedC =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Cherry",10000),executor);
+        Map<String,String> joined;
+
+        subject.put("A",expectedA);
+        subject.put("B",expectedB);
+        subject.put("C",expectedC);
+
+        joined = subject.joinAll();
+
+        assertEquals(3,joined.size());
+        assertEquals("Apple",joined.get("A"));
+        assertEquals("Banana",joined.get("B"));
+        assertEquals("Cherry",joined.get("C"));
+    }
+
+    @Test
+    public void
+    testJoinAll2() throws Exception
+    {
+        CompletionStage<String> expectedA =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Apple",100),executor);
+        CompletionStage<String> expectedB =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Banana",100),executor);
+        CompletionStage<String> expectedC =
+            CompletableFuture.supplyAsync(
+                () -> outputWithDelay("Cherry",10000),executor);
+        Map<String,String> joined;
+
+        subject.put("A",expectedA);
+        subject.put("B",expectedB);
+        subject.put("C",expectedC);
+
+        Thread.sleep(100);
+
+        joined = subject.joinAll();
+
+        assertEquals(1,joined.size());
+        assertEquals("Cherry",joined.get("C"));
+    }
+
+    private String
+    outputWithDelay(String value,long delayMillis)
+    {
+        try
+        {
+            Thread.sleep(delayMillis);
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        return value;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
