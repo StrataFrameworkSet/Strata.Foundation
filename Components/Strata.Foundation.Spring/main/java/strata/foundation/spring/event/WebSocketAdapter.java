2e4a03416c7ff6b545631fb8be984f6b94005abf
johnliebenau <johnliebenau@gmail.com>
2026-02-25 18:09:07 -0800

    Adding WebSocket module skeletons.

---
+++ Components/Strata.Foundation.Spring/main/java/strata/foundation/spring/event/WebSocketAdapter.java
@@ -0,0 +1,24 @@
+/// ///////////////////////////////////////////////////////////////////////////
+// WebSocketAdapter.java
+//////////////////////////////////////////////////////////////////////////////
+
+package strata.foundation.spring.event;
+
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+public
+class WebSocketAdapter<E>
+    implements WebSocketHandler
+{
+    @Override
+    public Mono<Void>
+    handle(WebSocketSession session)
+    {
+        session.se
+        return null;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
